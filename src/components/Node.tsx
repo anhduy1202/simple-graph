ca104c76dc4eecaafe1b60631bbca11d0a19c6df
Daniel Truong <aduy1122@gmail.com>
2022-04-10 12:27:21 -0700

    Replacing val with label, adding custom x,y coords

--- src/components/Node.tsx
+++ src/components/Node.tsx
@@ -1,28 +1,24 @@
 import React, { useEffect } from 'react';
-import { Canvas } from './Canvas';
 
 export interface NodeType {
-    val: string | number;
+    label: string | number;
+    x: number;
+    y: number;
 }
 
 export const Node: React.FC<NodeType> = (props) => {
-    const { val } = props;
+    const { label, x, y } = props;
 
     useEffect(() => {
-        var nodeNumber = document?.querySelectorAll('#node').length;
         const canvas: any = document?.getElementById('myCanvas');
         const ctx: any = canvas.getContext('2d');
         ctx.beginPath();
-        ctx.arc(nodeNumber + 75, nodeNumber + 75, 50, 0, Math.PI * 2, true);
+        ctx.arc(x, y, 50, 0, Math.PI * 2, true);
         ctx.lineWidth = 2;
         ctx.stroke();
         ctx.font = '30px Arial';
-        ctx.fillText(val, nodeNumber + 65, nodeNumber + 85);
+        ctx.fillText(label, x - 10, y + 10);
     }, []);
 
-    return (
-        <div id="node">
-            <Canvas></Canvas>
-        </div>
-    );
+    return <div id="node"></div>;
 };
